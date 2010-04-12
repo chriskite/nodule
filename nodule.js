e6a4ab1dcd3cd4e9560d5c3ca253165b2a39e268
Chris Kite <chris@chriskite.com>
2010-04-11 22:22:15 -0500

    created logger object

--- nodule.js
+++ nodule.js
@@ -1,39 +1,54 @@
 var net = require('net'),
     sys = require('sys');
 
+var LOGGING = true;
+
 var PORT = 13823;
 
 var STATUS_OK = 0;
 var E_INVALID_REQUEST = 1,
     E_UNKNOWN_CMD = 2;
 
+/*
+ * Handle logging to STDOUT if LOGGING == true.
+ */
+var logger = new function() {
+    this.log = function(msg) {
+        if(LOGGING) sys.log(msg);
+    };
+};
+
+/*
+ * Represent each entry in the key-value store.
+ */
 function Entry(value) {
     this.value = value;
     this.timestamp = new Date();
 }
-
 Entry.prototype.getValue = function() {
     return this.value;
 }
-
 Entry.prototype.setValue = function(value) {
     this.value = value;
     this.timestamp = new Date();
     return this;
 }
 
+/*
+ * Store the entries and mitigate access.
+ */
 var store = new function() {
     var entries = {};
 
     this.get = function(space, key) {
-        sys.log('Store: GET    Space:' + space + ' Key:' + key);
+        logger.log('Store: GET    Space:' + space + ' Key:' + key);
         entry = entries[key];
         if(!entry) return null;
         return entry.getValue();
     };
 
     this.set = function(space, key, value) {
-        sys.log('Store: SET    Space:' + space + ' Key:' + key + ' Value:' + value);
+        logger.log('Store: SET    Space:' + space + ' Key:' + key + ' Value:' + value);
         entry = entries[key];
         if(entry) {
             entry.setValue(value);
@@ -44,6 +59,9 @@ var store = new function() {
     };
 };
 
+/*
+ * Parse and process incoming requests.
+ */
 var processor = new function() {
     this.process = function(data, stream) {
         try {
@@ -58,13 +76,13 @@ var processor = new function() {
             case 'get': this.get(cmd, stream); break;
             case 'set': this.set(cmd, stream); break;
             default: 
-                sys.log('Processor: Unknown command "' + cmd['op'] + '"');
+                logger.log('Processor: Unknown command "' + cmd['op'] + '"');
                 this.error(E_UNKNOWN_CMD, stream);
         }
     };
     
     this.error = function(errno, stream) {
-        sys.log('Processor: ERROR ' + errno);
+        logger.log('Processor: ERROR ' + errno);
         this.sendAsJSON({'status': errno}, stream);
     };
     
@@ -73,31 +91,35 @@ var processor = new function() {
     };
     
     this.get = function(cmd, stream) {
-        sys.log('Processor: GET');
+        logger.log('Processor: GET');
         var value = store.get(cmd['space'], cmd['key']);
         this.sendAsJSON({'value': value}, stream);
     };
     
     this.set = function(cmd, stream) {
-        sys.log('Processor: SET');
+        logger.log('Processor: SET');
         var r = store.set(cmd['space'], cmd['key'], cmd['value']);
         this.sendAsJSON({'status': r}, stream);
     };    
 };
 
+/*
+ * Listen for connections and data events.
+ */
 var server = net.createServer(function (stream) {
   stream.setEncoding('utf8');
   stream.addListener('connect', function () {
-    sys.log('Server: CONNECT     ' + stream.remoteAddress);
+    logger.log('Server: CONNECT     ' + stream.remoteAddress);
   });
   stream.addListener('data', function (data) {
-    sys.log('Server: RECEIVE     ' + stream.remoteAddress + ' ' + data);
+    logger.log('Server: RECEIVE     ' + stream.remoteAddress + ' ' + data);
     processor.process(data, stream);
   });
   stream.addListener('end', function () {
-    sys.log('Server: DISCONNECT  ' + stream.remoteAddress);
+    logger.log('Server: DISCONNECT  ' + stream.remoteAddress);
     stream.end();
   });
 });
 
+// Start the server on PORT
 server.listen(PORT, "localhost");
