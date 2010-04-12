04b1277d854cb1ad13e018cfe2d6426fb1011b55
Chris Kite <chris@chriskite.com>
2010-04-11 23:19:20 -0500

    session tracking

--- nodule.js
+++ nodule.js
@@ -19,7 +19,7 @@ var logger = new function() {
 };
 
 /*
- * Represent each entry in the key-value store.
+ * An entry in the key-value store.
  */
 function Entry(value) {
     this.value = value;
@@ -35,20 +35,31 @@ Entry.prototype.setValue = function(value) {
 }
 
 /*
+ * A session for a connected client.
+ */
+function Session(stream) {
+    this.stream = stream;
+    this.id = Math.floor(Math.random()*99999999999).toString();
+}
+Session.prototype.sendAsJSON = function(data) {
+    this.stream.write(JSON.stringify(data) + "\n");
+};
+
+/*
  * Store the entries and mitigate access.
  */
 var store = new function() {
     var entries = {};
 
     this.get = function(space, key) {
-        logger.log('Store: GET    Space:' + space + ' Key:' + key);
+        logger.log('Store:     GET Space:' + space + ' Key:' + key);
         entry = entries[key];
         if(!entry) return null;
         return entry.getValue();
     };
 
     this.set = function(space, key, value) {
-        logger.log('Store: SET    Space:' + space + ' Key:' + key + ' Value:' + value);
+        logger.log('Store:     SET Space:' + space + ' Key:' + key + ' Value:' + value);
         entry = entries[key];
         if(entry) {
             entry.setValue(value);
@@ -63,43 +74,39 @@ var store = new function() {
  * Parse and process incoming requests.
  */
 var processor = new function() {
-    this.process = function(data, stream) {
+    this.process = function(data, session) {
         try {
             var cmd = JSON.parse(data);
         } catch(err) {
-            this.error(E_INVALID_REQUEST, stream);
+            this.error(E_INVALID_REQUEST, session);
             return;
         }
         if(!cmd.hasOwnProperty('op')) return;
         
         switch(cmd['op']) {
-            case 'get': this.get(cmd, stream); break;
-            case 'set': this.set(cmd, stream); break;
+            case 'get': this.get(cmd, session); break;
+            case 'set': this.set(cmd, session); break;
             default: 
                 logger.log('Processor: Unknown command "' + cmd['op'] + '"');
-                this.error(E_UNKNOWN_CMD, stream);
+                this.error(E_UNKNOWN_CMD, session);
         }
     };
     
-    this.error = function(errno, stream) {
+    this.error = function(errno, session) {
         logger.log('Processor: ERROR ' + errno);
-        this.sendAsJSON({'status': errno}, stream);
+        session.sendAsJSON({'status': errno});
     };
     
-    this.sendAsJSON = function(data, stream) {
-        stream.write(JSON.stringify(data) + "\n");
-    };
-    
-    this.get = function(cmd, stream) {
+    this.get = function(cmd, session) {
         logger.log('Processor: GET');
         var value = store.get(cmd['space'], cmd['key']);
-        this.sendAsJSON({'value': value}, stream);
+        session.sendAsJSON({'value': value});
     };
     
-    this.set = function(cmd, stream) {
+    this.set = function(cmd, session) {
         logger.log('Processor: SET');
         var r = store.set(cmd['space'], cmd['key'], cmd['value']);
-        this.sendAsJSON({'status': r}, stream);
+        session.sendAsJSON({'status': r});
     };    
 };
 
@@ -109,15 +116,18 @@ var processor = new function() {
 var server = net.createServer(function (stream) {
   stream.setEncoding('utf8');
   stream.addListener('connect', function () {
-    logger.log('Server: CONNECT     ' + stream.remoteAddress);
+    // create a session for this client
+    stream.session = new Session(stream);
+    logger.log('Server:    CONNECT    ' + stream.session.id + ' from ' + stream.remoteAddress);
   });
   stream.addListener('data', function (data) {
-    logger.log('Server: RECEIVE     ' + stream.remoteAddress + ' ' + data);
-    processor.process(data, stream);
+    logger.log('Server:    RECEIVE    ' + stream.session.id + ' from ' + stream.remoteAddress + ' ' + data);
+    processor.process(data, stream.session);
   });
   stream.addListener('end', function () {
-    logger.log('Server: DISCONNECT  ' + stream.remoteAddress);
+    logger.log('Server:    DISCONNECT ' + stream.session.id + ' from ' + stream.remoteAddress);
     stream.end();
+    delete stream.session;
   });
 });
 
