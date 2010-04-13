75d8c33005c635b0b3321333e22ffe312facf533
Chris Kite <chris@chriskite.com>
2010-04-12 21:17:13 -0500

    eventmachine in ruby client for pubsub

--- client/ruby/nodule.rb
+++ client/ruby/nodule.rb
@@ -1,32 +1,66 @@
 require 'rubygems'
 require 'json'
 require 'socket'
+require 'eventmachine'
 require 'pp'
 
-class Nodule
+module Nodule
 
-  def initialize(addr = 'localhost', port = 13823)
-    @socket = TCPSocket.new(addr, port)
-  end  
-  
-  def op(name, opts)
-    request = {:op => name}.merge(opts).to_json
-    @socket.print request
-    @socket.gets
+  class Client
+    def initialize(addr = 'localhost', port = 13823)
+      @socket = TCPSocket.new(addr, port)
+    end  
+    
+    def op(name, opts)
+      request = {:op => name}.merge(opts).to_json
+      @socket.print request
+      @socket.gets
+    end
+    
+    def get(space, key)
+      response = op(:get, {:space => space, :key => key})
+      JSON.parse(response)['value']
+    end
+    
+    def set(space, key, value)
+      response = op(:set, {:space => space, :key => key, :value => value})
+      self
+    end
+    
+    def subscription(&block)
+      EventMachine::run do
+        EventMachine::connect('localhost', 13823, Nodule::Subscription).instance_eval(&block)
+      end
+    end
+    
+    def destroy
+      @socket.close
+    end
   end
   
-  def get(space, key)
-    response = op(:get, {:space => space, :key => key})
-    JSON.parse(response)['value']
-  end
-  
-  def set(space, key, value)
-    response = op(:set, {:space => space, :key => key, :value => value})
-    self
-  end
-  
-  def destroy
-    @socket.close
+  module Subscription
+    def receive_data(data)
+      p data
+    end
+    
+    def subscribe(space, key, &block)
+      add_callback(space, key, block)
+      # send the subscription request
+      send_data({:op => :sub, :space => space, :key => key}.to_json)
+    end
+    
+    def add_callback(space, key, block)
+      @callbacks ||= {}
+      @callbacks[space] ||= {}
+      @callbacks[space][key] ||= []
+      @callbacks[space][key] << block
+    end
   end
   
 end
+
+n = Nodule::Client.new
+n.subscription do
+  subscribe('global', 'test') { |space, key, value| puts value }
+end
+
