7fb2b47f22296b970bb7d4c06d93719b1c0a218f
Chris Kite <chris@chriskite.com>
2010-04-12 22:46:33 -0500

    deleted test client scripts

--- client/ruby/client.rb
+++
@@ -1,4 +0,0 @@
-require 'nodule'
-
-n = Nodule::Client.new
-n.set('global', ARGV[0], ARGV[1])
--- client/ruby/subscribe.rb
+++
@@ -1,5 +0,0 @@
-require 'nodule'
-
-Nodule.subscription do
-  subscribe('global', '.*') { |space, key, value| puts value.upcase }
-end
